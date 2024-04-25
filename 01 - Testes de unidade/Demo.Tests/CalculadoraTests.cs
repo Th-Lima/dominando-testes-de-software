cf8888254c9f2796accf319fd51711eafd3e6917
Thales Lima <lthales53@gmail.com>
2024-04-25 17:04:22 -0300

    First commit

---
+++ 01 - Testes de unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(4, resultado);
+        }
+    }
+}
