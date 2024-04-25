f258d56e65df3a955955655174b7e9f97591adcc
Thales Lima <lthales53@gmail.com>
2024-04-25 17:21:26 -0300

    Básico dos testes

--- 01 - Testes de unidade/Demo.Tests/CalculadoraTests.cs
+++ 01 - Testes de unidade/Demo.Tests/CalculadoraTests.cs
@@ -14,5 +14,20 @@
             //Assert
             Assert.Equal(4, resultado);
         }
+
+        [Theory]
+        [InlineData(1,1, 2)]
+        [InlineData(0,1, 1)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total, resultado);
+        }
     }
 }
