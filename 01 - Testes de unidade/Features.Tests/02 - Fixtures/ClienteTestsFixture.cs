399ec50689a05554d5118b054419d940c10b43fe
Thales Lima <lthales53@gmail.com>
2024-05-03 16:29:58 -0300

    Utilizando fixtures

---
+++ 01 - Testes de unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,41 @@
+﻿using Features.Clientes;
+
+namespace Features.Tests._02___Fixtures
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    {
+        
+    }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            return new Cliente(
+                Guid.NewGuid(),
+                "Eduardo",
+                "Pires",
+                DateTime.Now.AddYears(-30),
+                "edu@edu.com",
+                true,
+                DateTime.Now);
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            return new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
